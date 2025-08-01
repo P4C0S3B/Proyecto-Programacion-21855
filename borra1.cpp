0f663c5a15593fc23d491e4d1053170fa32cdc9d
Francisco Laines <you@example.compacosebastian2002@hotmail.com>
2025-07-31 20:15:22 -0500

    main y preestructura

--- borra1.cpp
+++ borra1.cpp
@@ -1,1 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+using namespace std;
+struct paciente{
+    string cedula;
+    string nombre;
+    string apellido;
+    int dia, mes, anio, edad;
+    char sexo;
+    
+};
+
+int main(){
+    int op;
+    cout<<"=========================="<<endl;
+    cout<<"REGISTRO DE CITAS MEDICAS"<<endl;
+    cout<<"=========================="<<endl;
+    cout<<"Ingrese una opcion"<<endl;
+    cout<<"1.- Registro de un paciente nuevo."<<endl;
+    cout<<"2.- Consulta de informacion del paciente (Por numero de cedula)"<<endl;
+    cout<<"3.- Agregar informacion del paciente"<<endl;
+    cout<<"4.- Salir"<<endl;
+    do{
+        cout<<"Por favor, ingrese una de las opciones mostradas (1 - 4)"<<endl;
+        cin >>op;
+    } while(op<1 || op>4);
+    return 0;
+}
